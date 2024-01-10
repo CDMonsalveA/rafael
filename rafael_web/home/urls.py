d93fbc956c17ca037e09bf774bbc602fa952d7ec
Atomlejandro <atomlejandro@gmail.com>
2024-01-09 23:10:59 -0500

    Models, admin, blogs post

--- rafael_web/home/urls.py
+++ rafael_web/home/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('services/', views.services, name='services'),
     path('team/', views.team, name='team'),
     path('contact/', views.contact, name='contact'),
-    path('blog/', views.blog, name='blog'),
+    path("blog/", views.blog_index, name="blog_index"),
+    path("blog/post/<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("blog/category/<category>/", views.blog_category, name="blog_category"),
 
 ]
--- rafael_web/home/views.py
+++ rafael_web/home/views.py
@@ -2,22 +2,47 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from home.models import Post, Comment
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/base.html')
+    return render(request, 'base.html')
 
 def home(request):
-    return render(request, 'home/base.html')
+    return render(request, 'base.html')
 
 def services(request):
-    return render(request, 'home/base.html')
+    return render(request, 'base.html')
 
 def team(request):
-    return render(request, 'home/base.html')
+    return render(request, 'base.html')
 
 def contact(request):
-    return render(request, 'home/base.html')
+    return render(request, 'base.html')
 
-def blog(request):
-    return render(request, 'home/base.html')
+def blog_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts":posts,
+    }
+    return render(request, "home/blog_index.html", context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "home/category.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "home/blog_detail.html", context)
